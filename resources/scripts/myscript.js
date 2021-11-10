d04c50f46ed30c7d67113a319c539034745a6999
Arun Maurya <arunmaurya621@gmail.com>
2021-11-10 09:50:47 +0530

    Updated UI

---
+++ resources/scripts/myscript.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById("top-btn");
+
+// Get the <span> element that closes the modal
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
