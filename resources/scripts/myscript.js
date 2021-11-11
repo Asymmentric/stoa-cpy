08efe6226c15f7afc29fadaef4fe310d9576afeb
Arun Maurya <arunmaurya621@gmail.com>
2021-11-12 01:48:17 +0530

    Final-02

--- resources/scripts/myscript.js
+++ resources/scripts/myscript.js
@@ -1,1 +1,16 @@
-console.log("WWWWWWWWw");
+console.log("conn");
+const regBtn=document.getElementById('mid-btn');
+const regBtn1=document.getElementById('top-btn')
+const modal=document.getElementById('ggl-form')
+regBtn.addEventListener('click',()=>{
+  console.log('clcic');
+    if (modal.style.display!=='block') {
+      modal.style.display='block';
+    }else modal.style.display='none'
+})
+regBtn1.addEventListener('click',()=>{
+  console.log('clcic');
+    if (modal.style.display!=='block') {
+      modal.style.display='block';
+    }else modal.style.display='none'
+})
